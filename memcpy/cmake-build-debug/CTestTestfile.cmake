15f3d7648ab2af240f158efa0548097d87e145e4
Walingar <walingar@mail.ru>
2018-06-12 13:19:11 +0300

    Removed redundant files

--- memcpy/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/walingar/Documents/cpp-course/memcpy
-# Build directory: /home/walingar/Documents/cpp-course/memcpy/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(memcpy_unit_test "memcpy_unit_test")
-subdirs("googletest")
--- memcpy/cmake-build-debug/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/walingar/Documents/cpp-course/memcpy/googletest/googlemock
-# Build directory: /home/walingar/Documents/cpp-course/memcpy/cmake-build-debug/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
